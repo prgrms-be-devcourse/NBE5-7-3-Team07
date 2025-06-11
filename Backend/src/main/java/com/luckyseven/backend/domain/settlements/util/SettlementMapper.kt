b032ae23026cf99e5132875eab8eaad752764d98
KEEKE132 <dksvlfdhs1@gmail.com>
2025-06-11 14:26:45 +0900

    refactor: migrateSettlementMapper
    .java을(를) .kt(으)로 이름 변경

---
+++ Backend/src/main/java/com/luckyseven/backend/domain/settlements/util/SettlementMapper.kt
@@ -0,0 +1,51 @@
+package com.luckyseven.backend.domain.settlements.util;
+
+import com.luckyseven.backend.domain.expense.entity.Expense
+import com.luckyseven.backend.domain.member.entity.Member
+import com.luckyseven.backend.domain.settlements.dto.SettlementCreateRequest
+import com.luckyseven.backend.domain.settlements.dto.SettlementResponse
+import com.luckyseven.backend.domain.settlements.entity.Settlement
+import java.math.BigDecimal
+
+object SettlementMapper {
+
+    fun toSettlementResponse(settlement: Settlement): SettlementResponse {
+        return SettlementResponse.builder()
+            .id(settlement.id)
+            .amount(settlement.amount)
+            .createdAt(settlement.createdAt)
+            .updatedAt(settlement.updatedAt)
+            .isSettled(settlement.isSettled)
+            .settlerId(settlement.settler.id)
+            .settlerNickname(settlement.settler.nickname)
+            .payerId(settlement.payer.id)
+            .payerNickname(settlement.payer.nickname)
+            .expenseId(settlement.expense.id)
+            .expenseDescription(settlement.expense.description)
+            .teamId(settlement.expense.team.id)
+            .build()
+    }
+
+    fun fromSettlementCreateRequest(
+        request: SettlementCreateRequest,
+        settler: Member,
+        payer: Member,
+        expense: Expense
+    ): Settlement {
+        return Settlement(request.amount, settler, payer, expense)
+    }
+
+    fun toSettlementCreateRequest(
+        expense: Expense,
+        payerId: Long,
+        settlerId: Long,
+        shareAmount: BigDecimal
+    ): SettlementCreateRequest {
+        return SettlementCreateRequest.builder()
+            .expenseId(expense.id)
+            .payerId(payerId)
+            .settlerId(settlerId)
+            .amount(shareAmount)
+            .build()
+    }
+}
