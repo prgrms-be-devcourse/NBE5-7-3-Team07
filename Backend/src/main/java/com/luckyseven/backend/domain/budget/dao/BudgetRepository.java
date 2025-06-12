3ca311553446e98b78f4dfc358abee295fa52975
mipangg <morganjeong119@gmail.com>
2025-06-12 12:24:32 +0900

    refactor(Controller, Repository): BudgetController, Repository 코틀린 마이그레이션

--- Backend/src/main/java/com/luckyseven/backend/domain/budget/dao/BudgetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luckyseven.backend.domain.budget.dao;
-
-import com.luckyseven.backend.domain.budget.entity.Budget;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
-  Optional<Budget> findByTeamId(Long teamId);
-}
