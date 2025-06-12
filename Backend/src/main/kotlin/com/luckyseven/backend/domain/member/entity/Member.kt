3acce96370dbd27509e0e31b1d2f48ab154c9ef0
iseonbin <twinkey0201@gmail.com>
2025-06-12 14:50:41 +0900

    refactor(user) : migrate Member

---
+++ Backend/src/main/kotlin/com/luckyseven/backend/domain/member/entity/Member.kt
@@ -0,0 +1,21 @@
+package com.luckyseven.backend.domain.member.entity
+
+import com.luckyseven.backend.sharedkernel.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "member")
+data class Member(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    
+    @Column(length = 255, nullable = false)
+    val email: String,
+    
+    @Column(length = 255, nullable = false)
+    val password: String,
+    
+    @Column(length = 50, nullable = false)
+    val nickname: String
+) : BaseEntity() 
