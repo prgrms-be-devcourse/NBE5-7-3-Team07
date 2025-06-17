27033df50d8b1f16ac667acf41b9083ba6d07b1c
pbk2312 <pbk2312@inu.ac.kr>
2025-06-17 10:28:55 +0900

    refactor : `@EnableCaching` 만 적용

--- Backend/src/main/kotlin/com/luckyseven/backend/BackendApplication.kt
+++ Backend/src/main/kotlin/com/luckyseven/backend/BackendApplication.kt
@@ -3,11 +3,9 @@ package com.luckyseven.backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableCaching
 @SpringBootApplication
-@EnableJpaAuditing
 class BackendApplication
 
 fun main(args: Array<String>) {
