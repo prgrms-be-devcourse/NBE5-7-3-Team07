1dcde805e3a33280dd0186c0c786ef45bc4dcebe
iseonbin <twinkey0201@gmail.com>
2025-06-18 10:42:40 +0900

    feat(Jwt) : 스케줄링을 위한 설정 .

--- Backend/src/main/kotlin/com/luckyseven/backend/sharedkernel/jwt/entity/RefreshToken.kt
+++ Backend/src/main/kotlin/com/luckyseven/backend/sharedkernel/jwt/entity/RefreshToken.kt
@@ -1,18 +1,21 @@
 package com.luckyseven.backend.sharedkernel.jwt.entity
 
+import com.luckyseven.backend.sharedkernel.jwt.util.JwtTokenizer
 import jakarta.persistence.*
+import java.time.Instant
 
 @Entity
 @Table(name = "refresh_token", indexes = [
    Index(name = "idx_refresh_token_value", columnList = "tokenValue"),
    Index(name = "idx_refresh_token_user_id", columnList = "userId")
 ])
- class RefreshToken(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
-    
-    val userId: Long,
-    
-    val tokenValue: String
-) 
+class RefreshToken(
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   val id: Long? = null,
+   val userId: Long,
+   @Column(length = 2000)
+   val tokenValue: String,
+   val issuedAt: Instant = Instant.now(),
+   val expiresAt: Instant = Instant.now().plusMillis(JwtTokenizer.REFRESH_TOKEN_EXPIRE)
+)
